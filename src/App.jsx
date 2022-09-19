d09c9d49cb0bc9d72ff7ff60507d95335d6b71ac
Sam <bezzantsam@gmail.com>
2022-09-19 04:52:17 -0700

    copinents rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 import styles from "./style";
 
+import {
+  Navbar,
+  Hero,
+  Billing,
+  Business,
+  Clients,
+  CTA,
+  CardDeal,
+  Footer,
+  Stats,
+  Testimonials,
+} from "./components";
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
+    </div>
 
-      <div className={`bg-primary ${styles.paddingX}${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          Stats Business CardDeal Testimonials Clients CTA Footer
-        </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX}${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>

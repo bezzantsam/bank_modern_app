1d67da411c43b3c1faa3c60da25c6f77a60248bc
Sam <bezzantsam@gmail.com>
2022-09-19 07:03:13 -0700

    ul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 const Navbar = () => {
   return (
-    <nav className="w-full flex py-6 justfy-between items-center navbar"></nav>
+    <nav className="w-full flex py-6 justfy-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1"></ul>
+    </nav>
   );
 };
 

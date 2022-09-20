474e94b86ea9e91084ad57a1390c791ea14fa1e0
Sam <bezzantsam@gmail.com>
2022-09-20 05:38:36 -0700

    toggle menu logic for sm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justfy-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -9,12 +10,17 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
           >
             <a href={`#${nav.id}`}> {nav.title}</a>
           </li>
         ))}
       </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle ? close : menu} />
+      </div>
     </nav>
   );
 };

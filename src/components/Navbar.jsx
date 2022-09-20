aedd153d6efa47d87a44ff8cf1fc694216b8ec61
Sam <bezzantsam@gmail.com>
2022-09-20 05:21:16 -0700

    navbar with styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,16 @@ const Navbar = () => {
   return (
     <nav className="w-full flex py-6 justfy-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1"></ul>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}
+          >
+            <a href={`#${nav.id}`}> {nav.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };

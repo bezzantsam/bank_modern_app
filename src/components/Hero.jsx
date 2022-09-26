1b07ac49e9cc0e790b8df8ad2537c2db5c15198d
Sam <bezzantsam@gmail.com>
2022-09-26 05:26:18 -0700

    fiished component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,11 @@ const Hero = () => (
     <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px6`}>
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For{" "}
+          <span className="text-white">1 Month</span>
+          Account
+        </p>
       </div>
     </div>
   </section>

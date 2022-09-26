fb5b4b24c7a5c797fe0a4dc497d0e566b326dd23
Sam <bezzantsam@gmail.com>
2022-09-26 07:04:58 -0700

    Hero h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,18 @@ const Hero = () => (
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
         <p className={`${styles.paragraph} ml-2`}>
           <span className="text-white">20%</span> Discount For{" "}
-          <span className="text-white">1 Month</span>
-          Account
+          <span className="text-white">1 Month</span> Account
         </p>
       </div>
+      <div classame="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation</span>{" "}
+        </h1>
+      </div>
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
     </div>
   </section>
 );

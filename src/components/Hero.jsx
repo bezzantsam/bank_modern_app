531043241c44695a1c0c8db78c7789589dd5082c
Sam <bezzantsam@gmail.com>
2022-09-26 05:26:01 -0700

    discount component w style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px6`}>
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+      </div>
+    </div>
+  </section>
+);
 
 export default Hero;

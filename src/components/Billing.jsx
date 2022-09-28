08042f5f8f636f813e3aff04f2c7459f14bfd8c2
Sam <bezzantsam@gmail.com>
2022-09-28 10:25:23 -0700

    Billing partial

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-const Billing = () => {
-  return <div>Billing</div>;
-};
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+    </div>
+  </section>
+);
 
 export default Billing;

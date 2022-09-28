cf328e8e70aeb4982d50e8a2857de3b02fba53d2
Sam <bezzantsam@gmail.com>
2022-09-28 05:33:16 -0700

    button comp

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -10,11 +10,12 @@ const Business = () => {
           You do the business, <br className="sm:block hidden" />
           we'll handle the money.
         </h2>
-        <p className={`${styles.paragraph}`}>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           With the right credit card, you can improve your financial life by
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
+        <Button styles="mt-10" />
       </div>
     </section>
   );

dee28326960585b9c4fc4bb3494385d0e1d277cc
Sam <bezzantsam@gmail.com>
2022-09-28 10:16:36 -0700

    Business comp

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -4,17 +4,22 @@ import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
   <div
-    className={`flex flex-row p-6 rounded-[20px] ${index !==
-      features.length - 1} ? "mb-6" : "mb-0"} feature-card`}
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
   >
     <div
-      className={`w-[64px] h-[64px] rounded-full ${styles.flexCeter} bg-dimBlue`}
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
     >
-      <img src={icon} alt="icon" className="w-[50%] h[50%] object-contain" />
-      <div className="flex-1 flex flex-col ml-3">
-        <h4>{title}</h4>
-        <p>{content}</p>
-      </div>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
     </div>
   </div>
 );

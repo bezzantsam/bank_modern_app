a2b229b689d62d453b7d1f30ece6fea8de3efca6
Sam <bezzantsam@gmail.com>
2022-09-29 05:50:21 -0700

    quotes img

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { quotes } from "../assets";
 
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
-};
+const FeedbackCard = ({ content, name, title, image }) => (
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+    <img src={quotes} />
+  </div>
+);
 
 export default FeedbackCard;

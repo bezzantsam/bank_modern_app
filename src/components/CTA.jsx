0252fee162656c2969284459b632912fd12a3f3e
Sam <bezzantsam@gmail.com>
2022-09-29 07:01:07 -0700

    bg-cyan on hover

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section>
+    <div>
+      <h2>Let's try our service now!</h2>
+      <p>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+  </section>
+);
 
 export default CTA;

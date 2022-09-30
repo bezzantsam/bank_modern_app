bb42d23b346c3df2d8b1d575ab8b83e4fe48b38c
Sam <bezzantsam@gmail.com>
2022-09-30 05:10:18 -0700

    heading style fix

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => (
     className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
   >
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.headog2}>Let's try our service now!</h2>
+      <h2 className={styles.heading2}>Let's try our service now!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Everything you need to accept card payments and grow your business
         anywhere on the planet.
